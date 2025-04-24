818e1432043bf5789ca2d6eb82bcd01f93a02a02
IBRAHGITE <gite.ibrahim02@gmail.com>
2025-04-24 15:45:30 +0000

    Librairie

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,6 +1,8 @@
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="public/js/script.js"></script>
-<script src="cdn.datatables.net/2.2.2/js/dataTables.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
+<script href="cdn.datatables.net/2.2.2/js/dataTables.min.js"></script>
+<script href="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
+<script src="lib/tinymce/tinymce.min.js"></script>
+<script src="lib/tinymce/tinymce-jquery.min.js"></script>
 </html>
