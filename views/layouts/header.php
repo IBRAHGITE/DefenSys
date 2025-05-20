9b94430e939b1b7cb07bac63196a7aee379ba709
IBRAHGITE <gite.ibrahim02@gmail.com>
2025-05-20 15:20:49 +0000

    logo

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -4,7 +4,7 @@
     <!-- <link rel="stylesheet" src="../../public/bootsrap/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="assets/styles/style.css">
     <link rel="stylesheet" src="assets/bootsrap/style.css">
-    <link rel="icon" type="image/x-icon" href="/chemin/vers/favicon.ico">
+    <link rel="icon" href="public/images/Defensys-logo.png">
     <link href="lib/DataTables/datatables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
